2b790af7f6affded8aa1ef821c0f4eb611c2f641
wangliangliang <wll45312@gmail.com>
2017-07-17 15:59:27 +0800

    update demo

--- app/src/main/java/com/rong/sample/MainActivity.java
+++ app/src/main/java/com/rong/sample/MainActivity.java
@@ -9,6 +9,8 @@ import com.rongcapital.sdk.ui.RongSdk;
 import com.rongcapital.sdk.ui.util.DateUtils;
 
 public class MainActivity extends AppCompatActivity {
+    String customerNo = ""; // 请使用下发的客户号
+    String operator = "";   // 操作员号
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +21,8 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.pay).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                RongSdk.receipt(MainActivity.this, "青橙科技订单", "健身房费用", DateUtils.getCurrentDataString(), 1L);
+                RongSdk.receipt(MainActivity.this, "青橙科技订单", "健身房费用", DateUtils.getCurrentDataString(), 1L,
+                    customerNo, operator);
             }
         });
     }
